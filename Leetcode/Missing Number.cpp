a3d00aa993946494718ea42e6e5692befe922945
AnasRaxa <anonraza999@gmail.com>
2024-07-14 18:19:06 +0500

    leetcode day 1

---
+++ Leetcode/Missing Number.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        vector<int>v(n+1,-1);
+        for(int i =0;i<nums.size();i++){
+            v[nums[i]] = nums[i];
+        }
+        for(int i =0;i<v.size();i++){
+            if(v[i]==-1)return i;
+        }
+        return 0;
+    }
+};
+
+
+
+
+
+
+
+// sol 2
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        int ans =0;
+        for(int i =1;i<=n;i++){
+            ans = ans ^ i;
+        }
+        for(int i =0;i<nums.size();i++){
+            ans= ans^nums[i];
+        }
+        return ans;
+    }
+};
+
+
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        
+        int n = nums.size();
+        int Tsum = (n*(n+1))/2;
+        return  Tsum - accumulate(nums.begin(),nums.end(),0);
+        
+    }
+}; 
+
+
